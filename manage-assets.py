5300830447d938e96c590efb1b146cf6d9dd65f4
Gerlando Termini <gtermini@gc.cuny.edu>
2021-02-17 14:50:59 -0500

    Added new features to manage-assets script

--- manage-assets.py
+++ manage-assets.py
@@ -17,26 +17,26 @@ if path.exists( 'pickle/orders.pickle' ):
     with open( 'pickle/orders.pickle', 'rb' ) as f:
         orders = pickle.load( f )
 
-if ( path.exists( 'pickle/dataframe.pickle' ) ):
+if path.exists( 'pickle/dataframe.pickle' ):
     data = pd.read_pickle( 'pickle/dataframe.pickle' )
 
-if ( len( sys.argv ) > 1 ):
-    if ( sys.argv[ 1 ] == 'buy'  ):
+if len( sys.argv ) > 1:
+    if sys.argv[ 1 ] == 'buy':
         try:
             orders[ str( len( orders ) ) ] = asset( sys.argv[ 2 ], sys.argv[ 3 ], sys.argv[ 4 ], str( len( orders ) ) )
         except:
             print( 'Syntax: manage-asset.py buy ticker quantity price' )
             exit()
 
-    elif ( sys.argv[ 1 ] == 'sell' ):
+    elif sys.argv[ 1 ] == 'sell':
         try:
-            orders[ sys.argv[ 2 ] ].status = 'sold'
+            orders[ sys.argv[ 2 ] ].status = 'S'
         except:
             print( 'Error: asset not found' )
             exit()
 
         try:
-            if ( float( sys.argv[ 3 ] ) > 0 ):
+            if float( sys.argv[ 3 ] ) > 0:
                 orders[ sys.argv[ 2 ] ].profit = round( (  orders[ sys.argv[ 2 ] ].quantity * float( sys.argv[ 3 ] ) ) - (  orders[ sys.argv[ 2 ] ].quantity *  orders[ sys.argv[ 2 ] ].price ), 3 )
             else:
                 orders.pop( sys.argv[ 2 ] )
@@ -45,35 +45,65 @@ if ( len( sys.argv ) > 1 ):
             exit()
 
     # Change the status on an existing order
-    elif ( sys.argv[ 1 ] == 'update_status' ):
+    elif sys.argv[ 1 ] == 'update_status':
         try:
             orders[ sys.argv[ 2 ] ].status = str( sys.argv[ 3 ] )
         except:
             print( 'Error: asset not found' )
             exit()
 
-    # List all orders in the log
-    elif ( sys.argv[ 1 ] == 'list' ):
-        if ( len( orders ) > 0 ):
-            print( 'Asset Log -------------------------------' )
-            print( "{:<10}  {:<16}  {:<6}  {:<12}  {:<12}  {:<12}  {:<12}  {:<25}".format( 'Status', 'Date/Time', 'Ticker', 'Quantity', 'Price', 'Cost', 'Value', 'ID' ) )
+    elif sys.argv[ 1 ] == 'csv':
+        import csv
+        with open( 'orders.csv', 'w', encoding='utf8' ) as csv_file:
+            writer = csv.writer( csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL )
+            writer.writerow( [ 'Date and Time', 'Order ID', 'Status', 'Ticker', 'Quantity', 'Price', 'Cost', 'Current Value', 'Profit' ] )
             for a_asset in orders.values():
-                print( "{:<10}  {:<16}  {:<6}  {:<12}  {:<12}  {:<12}  {:<12}  {:<25}".format( str( a_asset.status ), a_asset.timestamp.strftime( '%Y-%m-%d %H:%M' ), str( a_asset.ticker ), str( a_asset.quantity ), str( a_asset.price ), str( round( a_asset.price * a_asset.quantity, 3 ) ), str( round( data.iloc[ -1 ][ a_asset.ticker ] * a_asset.quantity, 3 ) ), str( a_asset.order_id ) ) )
-        else:
-            print( 'No orders found.' )
-        exit()
+                row = [
+                    a_asset.timestamp.strftime( '%Y-%m-%d %H:%M' ),
+                    str( a_asset.order_id ),
+                    str( a_asset.status ),
+                    str( a_asset.ticker ),
+                    str( a_asset.quantity ),
+                    str( a_asset.price ),
+                    str( round( a_asset.price * a_asset.quantity, 3 ) )
+                ]
 
+                if a_asset.status == 'B':
+                    row.extend( [ str( round( data.iloc[ -1 ][ a_asset.ticker ] * a_asset.quantity, 3 ) ), 0 ] )
+                elif a_asset.status == 'S':
+                   row.extend( [ 0, str( a_asset.profit ) ] )
+                else:
+                    row.extend( [ 0, 0 ] )
+
+                writer.writerow( row )
+
+    # List all orders in the log
+    if len( orders ) > 0:
+        print( 'Asset Log -------------------------------' )
+        for a_asset in orders.values():
+            if len( sys.argv ) <= 2 or ( len( sys.argv ) > 2 and a_asset.status == sys.argv[ 2 ] ):
+                print( "Date and time: {}\nID: {}\nStatus: {}\nTicker: {}\nQuantity: {}\nPrice: $ {}\nCost: $ {}".format(
+                    a_asset.timestamp.strftime( '%Y-%m-%d %H:%M' ),
+                    str( a_asset.order_id ),
+                    str( a_asset.status ),
+                    str( a_asset.ticker ),
+                    str( a_asset.quantity ),
+                    str( a_asset.price ),
+                    str( round( a_asset.price * a_asset.quantity, 3 ) )
+                ) )
+
+                if a_asset.status == 'B':
+                    print( 'Current Value: $ ' + str( round( data.iloc[ -1 ][ a_asset.ticker ] * a_asset.quantity, 3 ) ) )
+                elif a_asset.status == 'S':
+                    print( 'Profit: $ ' + str( a_asset.profit ) )
+
+                print()
     else:
-        print( 'Syntax: manage-asset.py buy ticker quantity price | sell asset_id sale_price | update_status order_id status | list' )
-        exit()
-
-    print( 'New Asset Log ---------------------------' )
-    print( "{:<10}  {:<16}  {:<6}  {:<12}  {:<12}  {:<12}  {:<12}  {:<25}".format( 'Status', 'Date/Time', 'Ticker', 'Quantity', 'Price', 'Cost', 'Value', 'ID' ) )
-    for a_asset in orders.values():
-        print( "{:<10}  {:<16}  {:<6}  {:<12}  {:<12}  {:<12}  {:<12}  {:<25}".format( str( a_asset.status ), a_asset.timestamp.strftime( '%Y-%m-%d %H:%M' ), str( a_asset.ticker ), str( a_asset.quantity ), str( a_asset.price ), str( round( a_asset.price * a_asset.quantity, 3 ) ), str( round( data.iloc[ -1 ][ a_asset.ticker ] * a_asset.quantity, 3 ) ), str( a_asset.order_id ) ) )
-
-    with open( 'pickle/orders.pickle', 'wb' ) as f:
-        pickle.dump( orders, f )
+        print( 'No orders found.' )
 
 else:
     print( 'Syntax: manage-asset.py buy ticker quantity price | sell asset_id sale_price | update_status order_id status | list' )
+    exit()
+
+with open( 'pickle/orders.pickle', 'wb' ) as f:
+    pickle.dump( orders, f )
